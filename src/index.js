7c48ce486c756f7892c38983c9ea77d6efb3649e
nbky <nboukourray@gmail.com>
2017-07-12 06:27:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>React JS Boilerplate</h1>
+                <h1>A Simple React Boilerplate</h1>
             </div>
         );
     }
